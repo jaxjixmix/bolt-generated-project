a550c4910a07d1b768c326d898c0bda16c9e3c69
JaxJixMix <allanwebix@gmail.com>
2024-10-30 09:46:15 +1100

    Initial commit from your app

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
+
+export default defineConfig({
+  integrations: [tailwind(), image()],
+});
